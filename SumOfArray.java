aa63571cae6aa8edad87b7f0418628860113cad7
RaushanSKumar <60773257+RaushanSKumar@users.noreply.github.com>
2020-02-11 11:35:30 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,12 @@
+package Exam;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 5, 8, 9, 3};
+        int sum =0;
+        for(int i: array)
+            sum =sum+i;
+            System.out.println(sum);
+        }
+    }
+
